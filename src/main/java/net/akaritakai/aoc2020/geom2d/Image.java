90d9acd7f86b34a00da3b3cc5dea4e888bc13643
Justin Kaufman <akaritakai@gmail.com>
2020-11-17 15:11:11 -0600

    Simplify image rendering

--- src/main/java/net/akaritakai/aoc2020/geom2d/Image.java
+++ src/main/java/net/akaritakai/aoc2020/geom2d/Image.java
@@ -4,17 +4,20 @@ import java.util.Collection;
 import java.util.Set;
 import java.util.function.Function;
 import java.util.function.Predicate;
-import java.util.stream.Collectors;
-import java.util.stream.LongStream;
 
 public class Image {
     public static <T> String renderImage(Collection<T> objects, Function<T, String> renderer, long width, long height) {
         var it = objects.iterator();
-        return LongStream.range(0, height).boxed()
-                .map(y -> LongStream.range(0, width).boxed()
-                        .map(x -> renderer.apply(it.hasNext() ? it.next() : null))
-                        .collect(Collectors.joining()))
-                .collect(Collectors.joining("\n"));
+        var sb = new StringBuilder();
+        for (var y = 0; y < height; y++) {
+            for (var x = 0; x < width; x++) {
+                var item = it.hasNext() ? it.next() : null;
+                var render = renderer.apply(item);
+                sb.append(render);
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
     }
 
     public static String renderImage(Set<Point> points, Function<Point, String> renderer) {
@@ -22,24 +25,28 @@ public class Image {
         return renderImage(imageSize, renderer);
     }
 
-    public static String renderImage(Rectangle imageSize, Function<Point, String> renderer) {
-        return LongStream.range(imageSize.y(), imageSize.y() + imageSize.height()).boxed()
-                .map(y -> LongStream.range(imageSize.x(), imageSize.x() + imageSize.width()).boxed()
-                        .map(x -> new Point(x, y))
-                        .map(renderer)
-                        .collect(Collectors.joining()))
-                .collect(Collectors.joining("\n"));
+    public static String renderImage(ImageSize imageSize, Function<Point, String> renderer) {
+        var sb = new StringBuilder();
+        for (var y = imageSize.topEdge(); y >= imageSize.bottomEdge(); y--) {
+            for (var x = imageSize.leftEdge(); x <= imageSize.rightEdge(); x++) {
+                var point = new Point(x, y);
+                var render = renderer.apply(point);
+                sb.append(render);
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
     }
 
-    public static Rectangle imageSize(Set<Point> visiblePoints) {
+    private static ImageSize imageSize(Set<Point> visiblePoints) {
         return imageSize(visiblePoints, point -> true);
     }
 
-    public static Rectangle imageSize(Set<Point> points, Predicate<Point> isVisible) {
-        var minHeight = points.stream().filter(isVisible).mapToLong(Point::y).min().orElseThrow();
-        var maxHeight = points.stream().filter(isVisible).mapToLong(Point::y).max().orElseThrow();
-        var minWidth = points.stream().filter(isVisible).mapToLong(Point::x).min().orElseThrow();
-        var maxWidth = points.stream().filter(isVisible).mapToLong(Point::x).max().orElseThrow();
-        return new Rectangle(minWidth, minHeight, maxWidth - minWidth + 1, maxHeight - minHeight + 1);
+    private static ImageSize imageSize(Set<Point> points, Predicate<Point> isVisible) {
+        var minX = points.stream().filter(isVisible).mapToLong(Point::x).min().orElseThrow();
+        var maxX = points.stream().filter(isVisible).mapToLong(Point::x).max().orElseThrow();
+        var minY = points.stream().filter(isVisible).mapToLong(Point::y).min().orElseThrow();
+        var maxY = points.stream().filter(isVisible).mapToLong(Point::y).max().orElseThrow();
+        return new ImageSize(new Point(minX, minY), new Point(maxX, maxY));
     }
 }

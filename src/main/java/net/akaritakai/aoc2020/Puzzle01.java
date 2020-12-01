a7c8c7e98955fba881e9d1d90dc4c46c7e1c78c6
Justin Kaufman <akaritakai@gmail.com>
2020-12-01 00:37:06 -0600

    Update to Puzzle01 for correctness

--- src/main/java/net/akaritakai/aoc2020/Puzzle01.java
+++ src/main/java/net/akaritakai/aoc2020/Puzzle01.java
@@ -4,8 +4,9 @@ import com.google.common.collect.Sets;
 
 import java.util.Collection;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
-@SuppressWarnings("UnstableApiUsage")
 public class Puzzle01 extends AbstractPuzzle {
     public Puzzle01(String puzzleInput) {
         super(puzzleInput);
@@ -30,10 +31,18 @@ public class Puzzle01 extends AbstractPuzzle {
      * Finds a specified number of entries in the puzzle input that sum to 2020 and returns their product as a string.
      */
     private String specialEntriesProduct(int numEntries) {
-        var numbers = getPuzzleInput().lines().map(Long::parseLong).collect(Collectors.toSet());
-        var combos = Sets.combinations(numbers, numEntries);
-        var combo = combos.stream().filter(Puzzle01::hasSpecialSum).findAny().orElseThrow();
+        var combo = combinations(numEntries).filter(Puzzle01::hasSpecialSum).findAny().orElseThrow();
         return String.valueOf(product(combo));
+    }
+
+    /**
+     * Returns combinations of elements (including repeats) in the puzzle input of a given size.
+     */
+    private Stream<Collection<Long>> combinations(int numEntries) {
+        var numbers = getPuzzleInput().lines().map(Long::parseLong).collect(Collectors.toList());
+        var indexes = IntStream.range(0, numbers.size()).boxed().collect(Collectors.toSet());
+        @SuppressWarnings("UnstableApiUsage") var indexCombos = Sets.combinations(indexes, numEntries);
+        return indexCombos.stream().map(combo -> combo.stream().map(numbers::get).collect(Collectors.toList()));
     }
 
     /**
--- src/test/java/net/akaritakai/aoc2020/TestPuzzle01.java
+++ src/test/java/net/akaritakai/aoc2020/TestPuzzle01.java
@@ -19,6 +19,17 @@ public class TestPuzzle01 extends BasePuzzleTest {
     }
 
     @Test
+    public void testPart1Example2() {
+        // Ensure duplicate entries are handled
+        var puzzle = new Puzzle01("""
+                1000
+                1010
+                1010
+                """);
+        Assert.assertEquals(puzzle.solvePart1(), "1020100");
+    }
+
+    @Test
     public void testSolvePart1() throws Exception {
         var puzzle = new Puzzle01(getStoredInput(1));
         Assert.assertEquals(puzzle.solvePart1(), "440979");
@@ -35,6 +46,17 @@ public class TestPuzzle01 extends BasePuzzleTest {
                 1456
                 """);
         Assert.assertEquals(puzzle.solvePart2(), "241861950");
+    }
+
+    @Test
+    public void testPart2Example2() {
+        // Ensure duplicate entries are handled
+        var puzzle = new Puzzle01("""
+                672
+                672
+                676
+                """);
+        Assert.assertEquals(puzzle.solvePart2(), "305270784");
     }
 
     @Test

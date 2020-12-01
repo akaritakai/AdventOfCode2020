9b45c5b40d1011a1856e0c352be3c026ff2caafc
Justin Kaufman <akaritakai@gmail.com>
2020-12-01 14:47:46 -0600

    Additional test cases and comments

--- src/main/java/net/akaritakai/aoc2020/Puzzle01.java
+++ src/main/java/net/akaritakai/aoc2020/Puzzle01.java
@@ -3,6 +3,19 @@ package net.akaritakai.aoc2020;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
+/**
+ * In Day 1, we're tasked with finding a subset of size k from a set of numbers of size n that sum to a given value,
+ * and then return the subset's product.
+ *
+ * Such problems are typically called k-sum problems.
+ * For 2-sum and 3-sum (our task) the best known runtimes are O(n) and O(n^2) respectively.
+ *
+ * A more advanced problem in this category is whether any subset (of any size) from a set of numbers of size n will sum
+ * to a given value. This is known as the subset sum problem and is famously NP-complete.
+ *
+ * Note on bounds: The use of 32-bit signed/unsigned integers is sufficient here as: if the numbers are all positive,
+ * there does not exist any tuple {a, b, c} of positive integers such that a+b+c = 2020 and a*b*c > (2^31)-1.
+ */
 public class Puzzle01 extends AbstractPuzzle {
     public Puzzle01(String puzzleInput) {
         super(puzzleInput);
@@ -15,21 +28,19 @@ public class Puzzle01 extends AbstractPuzzle {
 
     @Override
     public String solvePart1() {
-        // This problem is effectively known as "2SUM"
-        // The naive algorithm requires O(n^2) but we can do it in O(n):
-        // The typical O(n) method is to stash all the numbers in a frequency map and then iterate over the key set (i)
-        // and test if 2020-i is also present in the key set.
-        // The frequency part is needed so that we can handle duplicates.
-        var numbers = getPuzzleInput().lines().map(Long::parseLong)
+        // 2-SUM:
+        // The naive algorithm performs in O(n^2) but we can do it in O(n) while handling duplicates:
+        // - Stash all the numbers in a frequency map
+        // - For every number n:
+        //   - If 2020-n is present in the key set then our subset is {n, 2020-n} if:
+        //     - n != 2020-n, or
+        //     - the frequency of n in our set is at least 2
+        var numbers = getPuzzleInput().lines().map(Integer::parseUnsignedInt)
                 .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
         for (var n1 : numbers.keySet()) {
             var n2 = 2020 - n1;
-            if (numbers.containsKey(n2)) {
-                if (n1 != n2) {
-                    return String.valueOf(n1 * n2);
-                } else if (numbers.get(n2) >= 2) {
-                    return String.valueOf(n1 * n2);
-                }
+            if (numbers.containsKey(n2) && (n1 != n2 || numbers.get(n2) >= 2)) {
+                return String.valueOf(n1 * n2);
             }
         }
         throw new IllegalStateException("Unable to find the solution");
@@ -37,12 +48,12 @@ public class Puzzle01 extends AbstractPuzzle {
 
     @Override
     public String solvePart2() {
-        // This problem is effectively known as "3SUM"
-        // The naive algorithm requires O(n^3) but we can do it in O(n^2):
-        // The typical O(n^2) method is to stash all the numbers in a hash set and then iterate over pairs i, j and test
-        // if 2020-i-j is in the hash set. However, that won't work for us as we might have duplicate entries.
-        // Instead, we will eat an O(n lg n) sort, and then do O(n^2) comparisons.
-        var numbers = getPuzzleInput().lines().map(Long::parseLong).sorted().collect(Collectors.toList());
+        // 3-SUM:
+        // The naive algorithm performs in O(n^3) but we can do it in O(n^2) while handling duplicates:
+        // - Sort the numbers (takes O(n lg n) time)
+        // - Intelligently test tuples using the ordered property in O(n^2) time
+        var numbers = getPuzzleInput().lines().map(Integer::parseUnsignedInt)
+                .sorted().collect(Collectors.toList());
         for (var i = 0; i < numbers.size() - 2; i++) {
             var n1 = numbers.get(i);
             var j = i + 1;
@@ -50,12 +61,13 @@ public class Puzzle01 extends AbstractPuzzle {
             while (j < k) {
                 var n2 = numbers.get(j);
                 var n3 = numbers.get(k);
-                if (n1 + n2 + n3 == 2020) {
-                    return String.valueOf(n1 * n2 * n3);
-                } else if (n1 + n2 + n3 > 2020) {
+                var sum = n1 + n2 + n3;
+                if (sum < 2020) {
+                    j++;
+                } else if (sum > 2020) {
                     k--;
                 } else {
-                    j++;
+                    return String.valueOf(n1 * n2 * n3);
                 }
             }
         }
--- src/test/java/net/akaritakai/aoc2020/TestPuzzle01.java
+++ src/test/java/net/akaritakai/aoc2020/TestPuzzle01.java
@@ -30,6 +30,91 @@ public class TestPuzzle01 extends BasePuzzleTest {
     }
 
     @Test
+    public void testPart1Example3() {
+        // Ensure duplicate entries are handled (alternate order)
+        var puzzle = new Puzzle01("""
+                1010
+                1000
+                1010
+                """);
+        Assert.assertEquals(puzzle.solvePart1(), "1020100");
+    }
+
+    @Test
+    public void testPart1Example4() {
+        // Ensure a size of 2 is handled
+        var puzzle = new Puzzle01("""
+                1009
+                1011
+                """);
+        Assert.assertEquals(puzzle.solvePart1(), "1020099");
+    }
+
+    @Test(expectedExceptions = {IllegalStateException.class})
+    public void testPart1Example5() {
+        // Ensure a size of 1 fails
+        var puzzle = new Puzzle01("""
+                2020
+                """);
+        puzzle.solvePart1();
+    }
+
+    @Test(expectedExceptions = {IllegalStateException.class})
+    public void testPart1Example6() {
+        // Ensure an input that does not satisfy part 1 fails
+        var puzzle = new Puzzle01("""
+                1010
+                1011
+                """);
+        puzzle.solvePart1();
+    }
+
+    @Test(expectedExceptions = {IllegalStateException.class})
+    public void testPart1Example7() {
+        // Ensure an input that does not satisfy part 1 fails (alternate)
+        var puzzle = new Puzzle01("""
+                1721
+                979
+                366
+                675
+                1456
+                """);
+        puzzle.solvePart1();
+    }
+
+
+    @Test
+    public void testPart1Example8() {
+        // Ensure a size of 2 with duplicates are handled
+        var puzzle = new Puzzle01("""
+                1010
+                1010
+                """);
+        Assert.assertEquals(puzzle.solvePart1(), "1020100");
+    }
+
+    @Test
+    public void testPart1Example9() {
+        // Ensure a pass if the subset is on the extremes of the array
+        var puzzle = new Puzzle01("""
+                1721
+                979
+                366
+                675
+                1456
+                299
+                """);
+        Assert.assertEquals(puzzle.solvePart1(), "514579");
+    }
+
+    @Test(expectedExceptions = {IllegalStateException.class})
+    public void testPart1Example10() {
+        // Ensure an empty input fails
+        var puzzle = new Puzzle01("");
+        puzzle.solvePart1();
+    }
+
+    @Test
     public void testSolvePart1() throws Exception {
         var puzzle = new Puzzle01(getStoredInput(1));
         Assert.assertEquals(puzzle.solvePart1(), "440979");
@@ -57,6 +142,93 @@ public class TestPuzzle01 extends BasePuzzleTest {
                 676
                 """);
         Assert.assertEquals(puzzle.solvePart2(), "305270784");
+    }
+
+    @Test
+    public void testPart2Example3() {
+        // Ensure duplicate entries are handled (alternate order)
+        var puzzle = new Puzzle01("""
+                672
+                676
+                672
+                """);
+        Assert.assertEquals(puzzle.solvePart2(), "305270784");
+    }
+
+    @Test(expectedExceptions = {IllegalStateException.class})
+    public void testPart2Example4() {
+        // Ensure a size of 2 fails
+        var puzzle = new Puzzle01("""
+                1009
+                1011
+                """);
+        puzzle.solvePart2();
+    }
+
+    @Test(expectedExceptions = {IllegalStateException.class})
+    public void testPart2Example5() {
+        // Ensure a size of 1 fails
+        var puzzle = new Puzzle01("""
+                2020
+                """);
+        puzzle.solvePart2();
+    }
+
+    @Test(expectedExceptions = {IllegalStateException.class})
+    public void testPart2Example6() {
+        // Ensure an input that does not satisfy part 2 fails
+        var puzzle = new Puzzle01("""
+                500
+                501
+                1020
+                """);
+        puzzle.solvePart2();
+    }
+
+    @Test(expectedExceptions = {IllegalStateException.class})
+    public void testPart2Example7() {
+        // Ensure an input that does not satisfy part 2 fails (alternate)
+        var puzzle = new Puzzle01("""
+                1721
+                979
+                366
+                299
+                1456
+                """);
+        puzzle.solvePart2();
+    }
+
+    @Test
+    public void testPart2Example8() {
+        // Ensure a size of 4 with duplicates are handled
+        var puzzle = new Puzzle01("""
+                672
+                500
+                672
+                676
+                """);
+        Assert.assertEquals(puzzle.solvePart2(), "305270784");
+    }
+
+    @Test
+    public void testPart2Example9() {
+        // Ensure a pass if the subset is on the extremes of the array
+        var puzzle = new Puzzle01("""
+                979
+                1721
+                1456
+                366
+                299
+                675
+                """);
+        Assert.assertEquals(puzzle.solvePart2(), "241861950");
+    }
+
+    @Test(expectedExceptions = {IllegalStateException.class})
+    public void testPart2Example10() {
+        // Ensure an empty input fails
+        var puzzle = new Puzzle01("");
+        puzzle.solvePart2();
     }
 
     @Test

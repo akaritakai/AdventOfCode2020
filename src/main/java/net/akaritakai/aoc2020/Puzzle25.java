55bfb40bf5c224fc2fd0f5420f16802f9ad5c165
Justin Kaufman <akaritakai@gmail.com>
2020-12-25 00:30:00 -0600

    Add day 25 comment

--- src/main/java/net/akaritakai/aoc2020/Puzzle25.java
+++ src/main/java/net/akaritakai/aoc2020/Puzzle25.java
@@ -1,5 +1,18 @@
 package net.akaritakai.aoc2020;
 
+/**
+ * In Day 25, we are told to crack a key exchange algorithm.
+ *
+ * This algorithm is fundamentally a Diffie-Hellman key exchange with modulus p = 20201227 and base g = 7.
+ * So, the card's public key 'C' is derived from its private key 'c' as:
+ * C = g^c mod p
+ * And the door's public key 'D' is derived from its private key 'd' as:
+ * D = g^d mod p
+ * Where the shared secret is:
+ * C^d mod p = g^(cd) mod p = g^(dc) mod p = D^c mod p
+ *
+ * Solving this quickly is a hard problem, but our input is small enough that brute force suffices.
+ */
 public class Puzzle25 extends AbstractPuzzle {
     public Puzzle25(String puzzleInput) {
         super(puzzleInput);
